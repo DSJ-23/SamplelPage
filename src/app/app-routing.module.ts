a8ada9557222d205b16bfe694da408f76e2cd6dc
Daniel Silva <dannysilva12345@gmail.com>
2020-08-02 00:10:53 -0400

    navbar simple routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from '../app/components/first/first.component'
+import { SecondComponent } from './components/second/second.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "first", component: FirstComponent},
+  {path: "second", component: SecondComponent},
+  { path: '**', redirectTo: '/first', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
